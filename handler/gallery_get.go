3a42d3d48abfc71181503c7291c16af02655b6aa
SkyZhangT <36461750+SkyZhangT@users.noreply.github.com>
2021-04-18 14:18:59 -0700

    fix skip

--- handler/gallery_get.go
+++ handler/gallery_get.go
@@ -12,19 +12,18 @@ import (
   
 func GalleryGet(db database.DBInterface) gin.HandlerFunc{
 	return func(c *gin.Context){
-		fmt.Println(c.ClientIP())
-		fmt.Println(c.RemoteIP())
-		offset := c.Param("skip")
-		
+		offset := c.GetHeader("skip")
 		if offset == "" {
-			offset = "1"
+			offset = "0"
 		}
+
 		n, err := strconv.ParseInt(offset, 10, 64)
 		if err != nil {
 			fmt.Printf("Int64 conversion failed. value: %d", n)
 			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 			return
 		}
+
 		res, err := db.NextTen(n)
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
